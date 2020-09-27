a9b638cca389751a2cc2df1fa19e52bb0729e99b
fitg <gustaw.fit@gmail.com>
2020-09-27 15:33:45 +0100

    Update labirynth.py

--- labirynth.py
+++ labirynth.py
@@ -20,7 +20,6 @@
 import sys
 import math
 
-INFINITY = float('inf')
 BFS = 1
 
 # quick debug method as codingame does not display stdout
@@ -34,9 +33,7 @@ class Game:
     def get_destination(self, start, back):
         if not back:
             go_to = self.strategy.search(start, '?')
-            if go_to == None:
-                return self.strategy.search(start, 'C')
-            return go_to
+            return go_to if go_to else self.strategy.search(start, 'C')
         else:
             return self.strategy.search(start, 'T')
 
